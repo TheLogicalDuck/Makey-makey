98558d3030e7e918bd42931eb97ad8677eb7c288
TheLogicalDuck <patugo36@gmail.com>
2025-09-29 15:25:54 -0600

    Added venv and main flet code

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import flet as ft
+from flet_audio import Audio
+import threading
+
+# Normalizar las teclas
+def normalizar_tecla(k: str) -> str:
+    k = (k or "").lower()
+    if k== "space":
+        return" "
+    return k.replace(" ", "").replace("-", "")
+
